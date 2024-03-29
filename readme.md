9445f38a8bb6b57ef2f62beece5f373705d05d49
ToKiiO <bilal.dira69@gmail.com>
2024-03-29 12:04:36 +0100

    Readme et optimisation

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Projet Pokedex
+
+Le projet Pokedex est une application web conçue pour les amateurs de Pokémon. Il permet aux utilisateurs de rechercher et d'explorer différentes informations sur les Pokémon, tels que leurs caractéristiques, leurs types, leurs évolutions et bien plus encore.
+
+## Fonctionnalités
+
+- Recherche de Pokémon : Les utilisateurs peuvent rechercher des Pokémon spécifiques en utilisant leur nom.
+- Détails des Pokémon : Une fois qu'un Pokémon est sélectionné, les utilisateurs peuvent accéder à des informations détaillées telles que les statistiques, et les capacités.
+- Galerie d'images : Les utilisateurs peuvent visualiser des images haute résolution des Pokémon pour mieux les connaître.
+
+## Technologies utilisées
+
+- HTML, CSS et JavaScript pour la structure, le style et l'interactivité de l'application.
+- API Pokémon : L'application utilise une API de "pokeapi" pour récupérer les données sur les Pokémon.
--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
+//initialisation des variables
 tab = [];
 const button = document.getElementById("button");
 const card = document.getElementById("card");
+
+//fetch de l'api pour récupérer les noms des pokemons
 fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
-  .then((response) => response.json())
+  .then((response) => response.json()) //transforme la réponse en json
   .then((data) => {
+    //récupère les données
     data.results.forEach((pokemon) => {
-      tab.push(pokemon.name);
+      tab.push(pokemon.name); //ajoute les noms des pokemons dans le tableau tab
     });
   })
   .catch((error) => {
@@ -13,12 +17,16 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
   });
 console.log(tab);
 
+//fonction qui permet de récupérer les informations du pokemon
 button.addEventListener("click", () => {
+  //ajoute un écouteur d'événement click sur le bouton
   const input = document.getElementById("rech").value;
   if (tab.includes(input)) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
+    //vérifie si le nom du pokemon est dans le tableau tab
+    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`) //fetch de l'api pour récupérer les informations du pokemon si il est dans tab
       .then((response) => response.json())
       .then((data) => {
+        //affichage des informations du pokemon
         console.log(data);
         const name = data.name;
         const img =
@@ -40,6 +48,7 @@ button.addEventListener("click", () => {
           movesTab.push(move.move.name);
         });
         console.log(movesTab);
+        //affichage des informations dans le site
         document.getElementById("name").innerHTML = name;
         document.getElementById("img").src = img;
         document.getElementById("type").innerHTML += `<li>Type : ${type}</li>`;
@@ -64,9 +73,9 @@ button.addEventListener("click", () => {
         card.style.display = "block";
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error("Error:", error); //affiche une erreur si le pokemon n'existe pas
       });
   } else {
-    alert("Pokemon inéxistant");
+    alert("Pokemon inéxistant"); //affiche une alerte si le pokemon n'existe pas
   }
 });
